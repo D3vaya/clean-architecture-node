c747115727069340961b09459fb5ae166b7d58e7
D3vaya <j.ayala.higuera@gmaial.com>
2024-06-19 22:16:58 -0400

    init project

---
+++ src/domain/entities/log.entity..ts
@@ -0,0 +1,23 @@
+export enum LogSeverityLevel {
+  low = "low",
+  medium = "medium",
+  high = "high",
+}
+
+export class LogEntity {
+  public level: LogSeverityLevel;
+  public message: string;
+  public createdAt: Date;
+
+  constructor(message: string, level: LogSeverityLevel, createdAt: Date) {
+    this.message = message;
+    this.level = level;
+    this.createdAt = createdAt;
+  }
+
+  static fromJson(json: string): LogEntity {
+    const { message, level, createdAt } = JSON.parse(json);
+    const log = new LogEntity(message, level, new Date(createdAt));
+    return log;
+  }
+}
